edefee88f1163df3d3bc92ee24cda5786de44388
glenn <glenn@DESKTOP-PK8PU62>
2021-07-07 14:46:35 -0600

    Add project files.

---
+++ HRManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HRManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
